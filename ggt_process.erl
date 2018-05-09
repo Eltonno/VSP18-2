53effd73923a061768ac401ea482806ab3f0bc8c
Florian Weiß <dragonu1@web.de>
2018-05-09 14:14:29 +0200

    changes in the mayor files

--- ggt_process.erl
+++ ggt_process.erl
@@ -5,7 +5,7 @@ start(ArbeitsZeit,TermZeit,Quota,Praktikumsgruppe,Teamnummer,GGTProzessnummer,St
   GGTName = list_to_atom(integer_to_list(Praktikumsgruppe) ++ integer_to_list(Teamnummer) ++ integer_to_list(GGTProzessnummer) ++ integer_to_list(Starternummer)),
   ActVal = 	[{ggtname, GGTName},
     {mi, undefined},
-    {leftn, undefined},
+    {leftn, '1232'},
     {rightn, undefined},
     {arbeitszeit, ArbeitsZeit},
     {termzeit, TermZeit},
@@ -25,13 +25,13 @@ init_ggt(ActVal) ->
   util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ "\n"),
   {koordinator, Koordinator} = lists:keyfind(koordinator, 1, ActVal),
   Koordinator ! {hello, GGTName},
-  util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", "ggt-" ++ util:to_String(GGTName) ++ "hat sich am Koordinator angemeldet\n"),
+  util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " hat sich am Koordinator angemeldet\n"),
   net_adm:ping(NameServiceNode),
   NameService = global:whereis_name(nameservice),
   register(GGTName, self()),
   NameService ! {self(),{rebind,GGTName,node()}},
   receive
-    ok -> util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", "ggt-" ++ util:to_String(GGTName) ++ "hat sich am Namensdienst angemeldet\n")
+    ok -> util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " hat sich am Namensdienst angemeldet\n")
   end,
   {ok, TRef} = timer:apply_after(TermZeit,?MODULE,term_request,[ActVal]),
   messages(lists:keystore(nameservice, 1,lists:keystore(tref, 1, ActVal, {tref, TRef}),{nameservice, NameService})).
@@ -52,6 +52,9 @@ messages(ActVal) ->
   {nameservice, NameService} = lists:keyfind(nameservice, 1, ActVal),
   receive
     {setneighbors,LeftN,RightN} ->
+
+      util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " setneighbors -> LeftN: " ++ atom_to_list(LeftN) ++ "\n"),
+
       LActVal = lists:keystore(leftn, 1, ActVal, {leftn, LeftN}),
       util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " RightN: " ++ atom_to_list(LActVal) ++ "\n"),
       RLActVal = lists:keystore(rightn, 1, LActVal, {rightn, RightN}),
@@ -65,7 +68,7 @@ messages(ActVal) ->
       LastTMActVal = lists:keystore(letztenachricht, 1, TMActVal, {letztenachricht, vsutil:getUTC()}),
       messages(LastTMActVal);
     {sendy, Y} ->
-      util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", "setpm: " ++ integer_to_list(Y) ++ "\n"),
+      util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " " ++ integer_to_list(Y) ++ "\n"),
       util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " LeftN: " ++ atom_to_list(LeftN) ++ "\n"),
       util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " RightN: " ++ atom_to_list(RightN) ++ "\n"),
       if
@@ -73,12 +76,12 @@ messages(ActVal) ->
           {ok, cancel} = timer:cancel(TRef),
           NewMi = ((Mi-1) rem Y) + 1,
           MActVal = lists:keystore(mi, 1, ActVal, {mi, NewMi}),
-          NameService ! {self(),{lookup, LeftN}},
+          {nameservice, list_to_atom("auskunft@Mato.local")} ! {self(),{lookup, LeftN}},
           receive
             not_found -> util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " LeftN not found\n");
             {pin, LeftNPID} -> LeftNPID ! {sendy, NewMi}
           end,
-          NameService ! {self(),{lookup, RightN}},
+          {nameservice, list_to_atom("auskunft@Mato.local")} ! {self(),{lookup, RightN}},
           receive
             not_found -> util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " RightN not found\n");
             {pin, RightNPID} -> RightNPID ! {sendy, NewMi}
@@ -88,10 +91,10 @@ messages(ActVal) ->
           {ok, CTRef} = timer:apply_after(TermZeit,?MODULE,term_request, [MActVal]),
           TMActVal = lists:keystore(tref, 1, MActVal, {tref, CTRef}),
           LastTMActVal = lists:keystore(letztenachricht, 1, TMActVal, {letztenachricht, vsutil:getUTC()}),
-          util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ "hat sein Mi upgedatet zu: " ++ integer_to_list(NewMi) ++ "\n"),
+          util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log",util:to_String(GGTName) ++ " hat sein Mi upgedatet zu: " ++ integer_to_list(NewMi) ++ "\n"),
           messages(LastTMActVal);
         true ->
-          util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", "Keine Änderung am Mi\n"),
+          util:logging("GGTP_" ++ atom_to_list(GGTName) ++ "@" ++ atom_to_list(node()) ++ ".log", util:to_String(GGTName) ++ " Keine Änderung am Mi\n"),
           messages(ActVal)
       end;
     {From,{vote,Initiator}} ->
